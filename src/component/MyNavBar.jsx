0476128c7f76f60efe7a2b6ee75c6e5b443bf857
Luca-Dipietro <luca.dipietro.ld@gmail.com>
2024-04-24 09:55:23 +0200

    fix

--- src/component/MyNavBar.jsx
+++ src/component/MyNavBar.jsx
@@ -4,19 +4,21 @@ import Nav from "react-bootstrap/Nav";
 
 const MyNavBar = (props) => {
   return (
-    <Navbar expand="lg" className="bg-warning">
-      <Container fluid>
-        <Navbar.Brand href="/">{props.brandName}</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ms-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/">About</Nav.Link>
-            <Nav.Link href="/">Browser</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <>
+      <Navbar expand="lg" className="bg-body-tertiary">
+        <Container fluid>
+          <Navbar.Brand href="/">{props.brandName}</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/">About</Nav.Link>
+              <Nav.Link href="/">Browser</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
   );
 };
 
